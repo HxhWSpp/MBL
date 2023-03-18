26fd9c862f48877bf9cff43cbd6dabbaffbb8d40
none <none>
2023-03-18 10:04:51 +0200

    add custom user entity

---
+++ Data/Entities/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MBL.Data.Entities
+{
+    public class AppUser : IdentityUser
+    {
+        public AppUser()
+        {
+            ReadBooks = new HashSet<Book>();
+            ToReadBooks = new HashSet<Book>();
+        }
+        public virtual ICollection<Book> ReadBooks { get; set; }
+
+        
+        public virtual ICollection<Book> ToReadBooks { get; set; }
+    }
+}

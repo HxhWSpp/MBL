8716e079b22b08c2f0cca81c6ed43774b85a3fc8
Wsupp <Wsupp@DESKTOP-NIKHBB5>
2023-03-18 07:52:06 +0200

    Add project files.

---
+++ Data/Book.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace MBL.Data
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string imageLink { get; set; }
+        public string Language { get; set; }       
+        public string Link { get; set; }
+        public string Pages { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public int BookListId { get; set; }
+    }
+}

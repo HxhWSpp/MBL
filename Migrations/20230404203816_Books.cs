b6ceba82ea3488580caf7ee76777b9b16285e2ea
Wsupp p <wsuppbruh@gmail.com>
2023-04-06 22:07:36 +0300

    books now have genre

---
+++ Migrations/20230404203816_Books.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MBL.Migrations
+{
+    public partial class Books : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageLink",
+                table: "Books",
+                newName: "ImagePath");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Genre",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Genre",
+                table: "Books");
+
+            migrationBuilder.RenameColumn(
+                name: "ImagePath",
+                table: "Books",
+                newName: "ImageLink");
+        }
+    }
+}
--- Models/BookModel.cs
+++ Models/BookModel.cs
@@ -1,10 +1,10 @@
-﻿using System.Reflection.Metadata.Ecma335;
+﻿using MBL.Data.Entities;
 
-namespace MBL.Data.Entities
+namespace MBL.Models
 {
-    public class Book : BaseEntity
+    public class BookModel : BaseEntity
     {
-        public Book()
+        public BookModel()
         {
             ReadBooks = new HashSet<UserReadBook>();
             WantedBooks = new HashSet<UserWantedBook>();
@@ -13,15 +13,14 @@ namespace MBL.Data.Entities
 
 
         public virtual ICollection<UserWantedBook> WantedBooks { get; set; }
-    
+
         public string Author { get; set; }
-        public string ImagePath { get; set; }
+        public IFormFile Image { get; set; }
         public string Language { get; set; }
         public string Link { get; set; }
         public string Pages { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Genre { get; set; }
-
     }
 }
